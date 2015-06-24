d0fe9d94b4c844e25e0ac61ab26a5ceaabce716a
gakey <inagak3@gmail.com>
2015-06-25 01:05:12 +0900

    initial commit

---
+++ src/main/java/calc/initializer/WebAppInitializer.java
@@ -0,0 +1,23 @@
+package calc.initializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import calc.config.AppWebConfig;
+public class WebAppInitializer implements WebApplicationInitializer {
+	public void onStartup(ServletContext servletContext) throws ServletException {
+
+		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(AppWebConfig.class);
+        ctx.setServletContext(servletContext);
+        Dynamic dynamic = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+        dynamic.setLoadOnStartup(1);
+        dynamic.addMapping("/");
+
+   }
+}
